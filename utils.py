90d956496c0a7848d54e589c92caf96b631fdc11
stacziu <272537@student.pwr.edu.pl>
2025-05-07 16:17:29 +0200

    Fixed issues

--- utils.py
+++ utils.py
@@ -1,13 +1,29 @@
-# Exemplary calculator functions
+"""
+Exemplary calculator functions
+"""
 
-def add (a: int , b: int) -> int :
+
+def add(a: int, b: int) -> int:
+    """Sum of two numbers"""
+
     return a + b
 
-def subtract (a: int , b: int) -> int :
+
+def subtract(a: int, b: int) -> int:
+    """Difference of two numbers"""
+
     return a - b
 
-def multiply (a: int , b: int) -> int :
+
+def multiply(a: int, b: int) -> int:
+    """Multiplication of two numbers"""
+
     return a * b
 
-def divide ( a: int , b : int ) -> float :
+
+def divide(a: int, b: int) -> float:
+    """Division of two numbers"""
+
     return a / b
+
+def convert
